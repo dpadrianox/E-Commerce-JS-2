c955551030ca079be8bce4e6337577f32403d58c
Princess <dpadrianox@gmail.com>
2025-05-20 09:03:15 -0400

    displaying price

--- books.js
+++ books.js
@@ -1,62 +1,65 @@
 function renderBooks(filter) {
-  const booksWrapper = document.querySelector('.books')
+  const booksWrapper = document.querySelector(".books");
 
   const books = getBooks();
 
-  if (filter === 'LOW_TO_HIGH') {
-  books.sort((a, b) => a.originalPrice - b.originalPrice);
+  if (filter === "LOW_TO_HIGH") {
+    books.sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice));
+  } else if (filter === "HIGH_TO_LOW") {
+    books.sort((a, b) => ((b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice)));
+  } else if (filter === "RATING") {
+    books.sort((a, b) => b.rating - a.rating);
   }
-  else if (filter === 'HIGH_TO_LOW') {
-  books.sort((a, b) => b.originalPrice - a.originalPrice);
-  }
-  else if (filter === 'RATING') {
-    books.sort((a, b) => a.rating - b.rating);
-    }
 
-let ratingHTML = '';
-let rating = 4
-
-for (let i = 0; i < Math.floor(rating); ++i) {
-  ratingHTML += '<i class="fas fa-star"></i>\n'
-}
-
-if (Number.isInteger(rating)) {
-  ratingHTML += '<i class="fas fa-star-half-alt"></i>\n';
-
-}
-
-console.log(ratingHTML)
-
-
-const booksHTML = books.map((book) => {
-  return `<div class="book">
+  const booksHTML = books
+    .map((book) => {
+      return `<div class="book">
     <figure class="book__img--wrapper">
       <img class="book__img" src="${book.url}" alt="" />
     </figure>
     <div class="book__title">${book.title}</div>
     <div class="book__ratings">
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star-half-alt"></i>
+     ${ratingsHTML(book.rating)}
     </div>
     <div class="book__price">
-      <span>$${book.originalPrice.toFixed(2)}</span>
+    ${priceHTML(book.originalPrice, book.salePrice)}
     </div>
-  </div>;`
-}).join("");
+  </div>;`;
+    })
+    .join("");
 
-booksWrapper.innerHTML = booksHTML;
+  booksWrapper.innerHTML = booksHTML;
+}
+
+function priceHTML(originalPrice, salePrice) {
+  if (!salePrice) {
+    return `$${originalPrice.toFixed(2)}`;
+  }
+  return `<span class="book__price--normal">$${originalPrice.toFixed(
+    2
+  )}</span> $${salePrice.toFixed(2)}`;
+
+  console.log(originalPrice, salePrice);
+}
+
+function ratingsHTML(rating) {
+  let ratingHTML = "";
+  for (let i = 0; i < Math.floor(rating); ++i) {
+    ratingHTML += '<i class="fas fa-star"></i>\n';
+  }
+  if (!Number.isInteger(rating)) {
+    ratingHTML += '<i class="fas fa-star-half-alt"></i>\n';
+  }
+  return ratingHTML;
 }
 
 function filterBooks(event) {
-    renderBooks(event.target.value)
-  }
+  renderBooks(event.target.value);
+}
 
 setTimeout(() => {
   renderBooks();
-})
+});
 
 // FAKE DATA
 function getBooks() {
